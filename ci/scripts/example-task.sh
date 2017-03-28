1afb78c512d0099a9b8bc499a95578187678d11e
Kostya Cholak <kostya.cholak@anvileight.com>
2017-03-28 13:49:09 +0300

    Update assets

--- ci/scripts/example-task.sh
+++ ci/scripts/example-task.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 set -x -e
 
-source project/ci/scripts/docker-lib.sh
-start_docker
-
-docker info
-
-docker load -i redis/image
-
-export user="Hello"
-echo $urser > /tmp/123
+touch 123.txt
 exit 0
 
